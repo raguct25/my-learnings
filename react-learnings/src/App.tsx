19ce6afb770986def336380388590a84cec66525
ragu <ragu.s@yavar.ai>
2024-11-29 13:56:42 +0530

    facebook embedded signup

--- react-learnings/src/App.tsx
+++ react-learnings/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import FetchComponent from "./fetch-component/FetchComponent";
+import FacebookComponent from "./meta/FacebookComponent";
 
 function App() {
   return (
     <div className="App">
-      <FetchComponent />
+      {/* <FetchComponent /> */}
+      <FacebookComponent />
     </div>
   );
 }
